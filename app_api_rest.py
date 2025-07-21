6ca6767941213d0b8b538e416b3d3440b700693e
Ravine Rodrigues <107724478+Ravine28@users.noreply.github.com>
2025-07-21 19:56:18 +0000

    cmmt#2

--- app_api_rest.py
+++ app_api_rest.py
@@ -3,11 +3,8 @@ from flask import Flask, request, jsonify
 import uuid
 from hashlib import sha256
 
-
 app = Flask(__name__)
-
-#lista simulada de usuários
-usuarios = []
+usuarios = [] #lista simulada de usuários
 
 #função para gerar o hash do CPF
 def hash_cpf(cpf):
@@ -62,8 +59,4 @@ def excluir_usuario(id):
     return jsonify({"message": "Usuário excluído com sucesso"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-#if __name__ == '__main__':
-#    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
